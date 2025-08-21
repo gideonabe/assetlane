cd55b2399abd7e5fe02fe51f18a680235720c7ff
Gideon Abe <gideonabe2020@gmail.com>
2025-08-22 00:27:40 +0100

    more commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-// import logo from '../../../public/favicon.png'
-import unlock from '../../../public/img/unlock.png'
+// import unlock from '../../../public/img/unlock.png'
 
 const Navbar = () => {
   return (
     <div className='mt-5 relative z-10 mx-auto bg-white gap-2 md:gap-6 flex justify-between items-center border-2 border-gray-100 rounded-full px-2 py-2'>
       <div className='flex gap-0.5 md:gap-1 ml-5 justify-center items-center'>
-        <Image src="/favicon.png" width={29} height={5} alt='Assetlane'/>
+
+        <div className='relative w-6 h-6'>
+          <Image 
+            src="/favicon.png" 
+            alt='Assetlane Logo' 
+            sizes='20px' 
+            fill 
+            className='object-contain'/>
+        </div>
+
+        {/* <Image src="/favicon.png" width={29} height={15} alt='Assetlane'/> */}
         <h1 className='text-sm md:text-[17px] font-bold'>Assetlane</h1>
       </div>
 
@@ -17,7 +26,16 @@ const Navbar = () => {
         <p>About</p>
         <p>Contact</p>
         <div className='flex gap-2 bg-blue-100 px-2 py-2 md:px-8 md:py-5 rounded-full justify-center items-center'>
-          <Image src={unlock} width={20} height={25} alt='Unlock'/>
+          {/* <Image src={unlock} width={20} height={25} alt='Unlock'/> */}
+
+          <div className='relative w-6 h-6'>
+          <Image 
+            src="/img/unlock.png"
+            alt='Unlock icon' 
+            sizes='15px' 
+            fill 
+            className='object-contain'/>
+        </div>
           <p className='text-white text-xs'>Start 14-Day Trial</p>
         </div>
       </div>
--- src/app/components/Values.jsx
+++ src/app/components/Values.jsx
@@ -9,25 +9,25 @@ const Values = () => {
           <h1 className='text-2xl font-semibold'>What truly makes Assetlane different</h1>
         </div>
 
-        <div className='flex gap-4 mt-4 '>
-          <div className='flex flex-col gap-6 bg-white px-10 py-6'>
-            <img src="/favicon.png" alt="Assetlane logo" width={35}/>
+        <div className='flex gap-4 mt-4 h-[450px]'>
+          <div className='flex flex-col gap-6 bg-white px-10 py-6 justify-between'>
+            <img src="/favicon.png" alt="Assetlane logo" width={35} height={5} className='mt-4'/>
 
             <p className='text-3xl font-semibold'>Smarter Listings Management</p>
             <p className='text-[16px]'>Browse, search, and filter property listings in seconds — with real-time updates, clean data views, and built-in status tracking for every listing.</p>
 
             <p className='btn-custom'>Try Assetlane</p>
           </div>
-          <div className='flex flex-col gap-6 bg-white px-10 py-6'>
-            <img src="/favicon.png" alt="Assetlane logo" width={35}/>
+          <div className='flex flex-col gap-6 bg-white px-10 py-6 justify-between'>
+            <img src="/favicon.png" alt="Assetlane logo" width={35} className='mt-4'/>
 
             <p className='text-3xl font-semibold'>Centralized Appointments</p>
             <p className='text-[16px]'>All your meetings, showings, and calls in one view. Agents can schedule, reschedule, and update appointments in real time.</p>
 
             <p className='btn-custom'>Try Assetlane</p>
           </div>
-          <div className='flex flex-col gap-6 bg-white px-10 py-6'>
-            <img src="/favicon.png" alt="Assetlane logo" width={35}/>
+          <div className='flex flex-col gap-6 justify-between bg-white px-10 py-6'>
+            <img src="/favicon.png" alt="Assetlane logo" width={35} height={5} className='mt-4'/>
 
             <p className='text-3xl font-semibold'>Live Reports & Insights</p>
             <p className='text-[16px]'>Visualize your team's performance, track sales, and understand what's working — all through clean, real-time reports that actually make sense.</p>
